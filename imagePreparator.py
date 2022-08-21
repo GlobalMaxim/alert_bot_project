bcc94e7575110224b3ca1b307a4a8fd8a0f31441
Ubuntu <ubuntu@ip-172-31-29-111.us-east-2.compute.internal>
2022-08-21 05:32:23 +0300

    image preparator

--- imagePreparator.py
+++ imagePreparator.py
@@ -3,6 +3,6 @@ from PIL import Image
 class ImagePreparator():
     def cutImage(self, imagePath):
         with Image.open(imagePath) as im:
-            box = (500, 0, 3350, 2100)
+            box = (200, 0, 1680, 1050)
             im_croped =  im.crop(box)
             im_croped.save('screenshot.png')
