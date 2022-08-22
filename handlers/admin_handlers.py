20376b1d8a5c98f6b668773a9da9a2c7c48aa61a
GlobalMaxim <verchenko1998@gmail.com>
2022-08-22 18:51:03 +0300

    admin handlers updated

--- handlers/admin_handlers.py
+++ handlers/admin_handlers.py
@@ -4,7 +4,7 @@ from typing import Union
 from telebot import dp, bot
 from aiogram.types import Message, BotCommand, BotCommandScopeChat, BotCommandScopeDefault,CallbackQuery
 from config import admin_id, OS
-from keyboards.default.menu import menu
+from keyboards.default.menu import menu, menu_2
 from aiogram.dispatcher.filters import Command, Text
 from db.database import Database
 from telegram_redis.redisPreparation import Redis_Preparation 
@@ -39,38 +39,68 @@ async def send_to_admin(dp):
 @dp.message_handler(commands=['show_users_count'])
 async def count_user(message: Message):
     if message.from_user.id in admin_id:
+        mail = Mailing()
+        is_user_uses_alert = mail.is_user_alert_active(message.from_user.id)
+        if is_user_uses_alert == True:
+            markup = menu_2
+        else:
+            markup = menu
         db = Database()
         count = db.count_users()
         db.close_connection()
-        await message.answer(text=f'Всего {count} пользователей')
+        await message.answer(text=f'Всего {count} пользователей', reply_markup=markup)
 
 @dp.message_handler(commands=['show_mails_count'])
 async def count_mails(message: Message):
     if message.from_user.id in admin_id:
         mail = Mailing()
+        is_user_uses_alert = mail.is_user_alert_active(message.from_user.id)
+        if is_user_uses_alert == True:
+            markup = menu_2
+        else:
+            markup = menu
+        mail = Mailing()
         mails_count = mail.get_number_mails()
-        await message.answer(text=f'Всего {mails_count} рассылок')
+        await message.answer(text=f'Всего {mails_count} рассылок', reply_markup=markup)
 
 @dp.message_handler(commands=['clear_mails_log'])
 async def count_mails(message: Message):
     if message.from_user.id in admin_id:
         mail = Mailing()
+        is_user_uses_alert = mail.is_user_alert_active(message.from_user.id)
+        if is_user_uses_alert == True:
+            markup = menu_2
+        else:
+            markup = menu
+        mail = Mailing()
         mail.clear_redis_statuses()
-        await message.answer(text=f'Редис очищен')
+        await message.answer(text=f'Редис очищен', reply_markup=markup))
 
 @dp.message_handler(commands=['save'])
 async def save(message: Message):
     if message.from_user.id in admin_id:
+        mail = Mailing()
+        is_user_uses_alert = mail.is_user_alert_active(message.from_user.id)
+        if is_user_uses_alert == True:
+            markup = menu_2
+        else:
+            markup = menu
         db = Database()
         values = db.save_data_to_db()
         db.close_connection()
-        await message.answer(f'Добавлено {values[0]} новых пользователей и обновлено {values[1]} пользователя')
+        await message.answer(f'Добавлено {values[0]} новых пользователей и обновлено {values[1]} пользователя', reply_markup=markup))
 
 @dp.message_handler(commands=['parse'])
 async def save(message: Message):
     if message.from_user.id in admin_id:
+        mail = Mailing()
+        is_user_uses_alert = mail.is_user_alert_active(message.from_user.id)
+        if is_user_uses_alert == True:
+            markup = menu_2
+        else:
+            markup = menu
         await parse_photo()
-        await message.answer('Фото обновлено')
+        await message.answer('Фото обновлено', reply_markup=markup))
 
 # @dp.message_handler(commands=['delete'])
 # async def reset(message: Message):
@@ -84,6 +114,12 @@ async def save(message: Message):
 @dp.message_handler(commands=['show_all_data'])
 async def show_all_info(message: Message):
     if message.from_user.id in admin_id:
+        mail = Mailing()
+        is_user_uses_alert = mail.is_user_alert_active(message.from_user.id)
+        if is_user_uses_alert == True:
+            markup = menu_2
+        else:
+            markup = menu
         r = Redis_Preparation()
         new_users =  r.get_count_new_users()
         db = Database()
@@ -97,7 +133,7 @@ async def show_all_info(message: Message):
         count = db.count_requests()
         if not count:
             count = 0
-        await message.answer(text=f'Всего {count} Запросов', reply_markup=menu)
+        await message.answer(text=f'Всего {count} Запросов', reply_markup=markup)
         db.close_connection()
 
 @dp.message_handler(commands=['show_all_requests_count'])
