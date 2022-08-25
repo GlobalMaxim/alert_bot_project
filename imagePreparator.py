ca3c83c8708cdf32744d1be49e234ac220226bc8
GlobalMaxim <verchenko1998@gmail.com>
2022-08-25 10:52:20 +0300

    git uodate

--- imagePreparator.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-
-class ImagePreparator():
-    def cutImage(self, imagePath):
-        with Image.open(imagePath) as im:
-            box = (200, 0, 1680, 1050)
-            im_croped =  im.crop(box)
-            im_croped.save('screenshot.png')
