b59dd3c6b884b2b2f9ef62bb922352ec5b029e99
GlobalMaxim <verchenko1998@gmail.com>
2022-09-10 23:15:57 +0300

    update notif

--- mailing/send_notification_to_chanel.py
+++ mailing/send_notification_to_chanel.py
@@ -4,8 +4,10 @@ from telegram_redis.redisPreparation import Redis_Preparation
 from config import NOTIFICATION_CHANEL_ID
 from aiogram.types import ParseMode
 
+from telegram_redis.redis_for_notification import get_updated_regions
+
 async def send_notification_to_chanel(bot):
-    regions = Redis_Preparation().get_updated_regions()
+    regions = get_updated_regions()
         # regions = api_parse_info()
     if len(regions) > 0:
         for region in regions:
---
+++ telegram_redis/redis_for_notification.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+import redis
+import json
+from telegram_redis.redisPreparation import Redis_Preparation
+from test import api_parse_info
+import logging
+def get_updated_regions():
+    logging.basicConfig(level=logging.WARNING, filename='log/notification-redis-log.txt')
+    try:
+        regionsRedis = Redis_Preparation()
+
+        with redis.Redis(db=5) as redis_client:
+            try:
+                regions_from_redis = json.loads(redis_client.get('updated_regs'))
+            except:
+                regions_from_redis = None
+
+            if regions_from_redis == None:
+                default = regionsRedis.get_regions_from_redis()
+                redis_client.set('updated_regs', json.dumps(default['regions']))
+                return default['regions']
+            else:
+                api_data = api_parse_info()
+                regions_from_api = regionsRedis.get_and_update_regions_from_redis(api_data)
+                regs_redis = regions_from_redis
+                changed_regs = []
+                # regions_from_redis = data.copy()
+                if len(regions_from_redis) > 0:
+                    for region_api in regions_from_api['regions']:
+                        for key, reg_redis in enumerate(regs_redis.copy()):
+                            if region_api['alert'] == True and region_api not in regs_redis:
+                                changed_regs.append(region_api)
+                                regs_redis.append(region_api)
+                            elif region_api['alert'] == False and region_api['name'] == reg_redis['name']:
+                                
+                                del regs_redis[key]
+                                changed_regs.append(region_api)
+                redis_client.set('updated_regs', json.dumps(regs_redis))
+                return changed_regs
+
+    except:
+        logging.exception('\n'+'Get updated regions error! ' + '\n' + str(datetime.now().strftime("%d-%m-%Y %H:%M"))+ '\n')
